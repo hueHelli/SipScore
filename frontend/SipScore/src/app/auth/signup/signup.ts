bd05208e3b02003fe3d638348946db5bdbaef848
NicolaBUZ <nicola.bianchini@buhlergroup.com>
2025-09-13 17:35:57 +0200

    made singup and login

--- frontend/SipScore/src/app/auth/signup/signup.ts
+++ frontend/SipScore/src/app/auth/signup/signup.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.scss'
+  styleUrl: './signup.scss',
 })
 export class Signup {
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {}
 
+  signupForm!: FormGroup;
+  errorMsg: string | null = null;
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      this.errorMsg = 'Ungültige Angabe';
+      this.cdr.detectChanges();
+      return;
+    } else {
+      console.log(this.signupForm);
+    }
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  passwordMatchValidator() {
+    if (
+      this.signupForm &&
+      this.signupForm.get('password')?.value !==
+        this.signupForm.get('confirmPassword')?.value
+    ) {
+      return { mismatch: true };
+    }
+    return null;
+  }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.required,
+        this.passwordMatchValidator.bind(this),
+      ]),
+    });
+  }
 }
